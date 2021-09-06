410abf7b2cc4c1e0e56911ee1093c1f2d7da3fd0
ldayananda <lahiru07@gmail.com>
2021-09-06 12:26:47 -0400

    initial app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import RecipeCollection from './pages/recipeCollection'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    </div>
+  );
+}
+
+export default App;
