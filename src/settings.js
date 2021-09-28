ff2d16611f0ca910d1511d30247466a827c872d7
ldayananda <lahiru07@gmail.com>
2021-09-28 02:23:23 -0400

    clean up

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,6 @@
 const RECIPTE_DB_HOST = "localhost"
 const RECIPE_DB_PORT = "3001"
 const RECIPE_DB_URL = "http://" + RECIPTE_DB_HOST + ":" + RECIPE_DB_PORT
-// http://[::1]:3001/ping
 
 export {
     RECIPE_DB_URL
